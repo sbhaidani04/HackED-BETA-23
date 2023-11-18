6fc4a692e3f47cc39f90da70f3c776372ebffdb0
Caly Zheng <calyzheng1@gmail.com>
2023-11-18 13:06:18 -0700

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,18 @@ def read_file(filename):
     return (lines)
 
 def find_letter(lines):
+    return 0
+
+def get_input():
+    # returns a list of letters the user does NOT have
+
+    #gets input from the user
+    letters = input("Enter letters that you do not have seperated by spaces (a b c)\n")
+    #splits at spaces and turns into a list of letters
+    letters = letters.split()
+    return letters
+
+
     
 
 
